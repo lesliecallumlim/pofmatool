e829f82d1f0d05572313106fa85904490496d9d1
Callum Lim <hello@lesliecallum.com>
2020-04-30 13:31:00 +0800

    build file

---
+++ build/precache-manifest.063dff5832fcb713f98291176ecf87cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761bb7cac422245b149732e6d00bbbfa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be4c9e19895ceb74edb9",
+    "url": "./static/css/main.ccd35ec0.chunk.css"
+  },
+  {
+    "revision": "1dfa4c4a3a8e633e7bd0",
+    "url": "./static/js/2.39fc97db.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.39fc97db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4c9e19895ceb74edb9",
+    "url": "./static/js/main.5ec20ead.chunk.js"
+  },
+  {
+    "revision": "f4cd6b583435ede5dc94",
+    "url": "./static/js/runtime-main.9e732324.js"
+  }
+]);

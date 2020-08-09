f0da498b86cfa30de34d9dafdb55d15bd3e18f9e
Callum Lim <hello@lesliecallum.com>
2020-08-09 19:51:37 +0800

    manifest

--- api/__init__.py
+++ api/__init__.py
@@ -7,22 +7,23 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import logging
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-jwt = JWTManager(app)
+app = Flask(__name__) 
+app.config.from_object(Config) # Config file
+db = SQLAlchemy(app) # DB through the SQL ORM
+migrate = Migrate(app, db) # Data migration as needed
+jwt = JWTManager(app) # For JWT tokens
 
 # Logfile to keep track of the various exceptions raised
 logging.basicConfig(filename='error.log',level=logging.INFO)
 
+# Basically a rate limiter
 limiter = Limiter(
     app,
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"]
 )
 
-#
+# Custom message for JWT
 @jwt.expired_token_loader
 def my_expired_token_callback(expired_token):
     token_type = expired_token['type']
